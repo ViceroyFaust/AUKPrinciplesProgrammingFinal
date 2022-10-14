7432ee9e9a76ac5d0f1995b57ee879694be7f57b
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-10-14 15:23:16 +0300

    Improve song class to abide by project requirement Prior to this change, the Song class was bare-bones and immutable, which is not what is required by the project directions.
    This change creates more methods and instance variables that abide by
    the project requirements.

--- src/Song.java
+++ src/Song.java
@@ -1,18 +1,18 @@
 
 final public class Song {
-    private String title;
+    private String title; // (1) Private instance variables
     private String author;
 
-    public int releaseYear; // Because the class is immutable, public variables are also final
-    public int length;
+    public int secondsLength; // (1) Public instance variables
+    public int releaseYear; // (6) Numeric property
 
-    public Song(String title, String author, int releaseYear, int length) { // Constructor initialises everything
+    public Song(String title, String author, int secondsLength, int releaseYear) { // Constructor initialises everything
         this.title = title;
         this.author = author;
+        this.secondsLength = secondsLength;
         this.releaseYear = releaseYear;
-        this.length = length;
     }
-    // Getters for private variables
+    // (2) Getters for private variables
     public String getTitle() {
         return title;
     }
@@ -20,12 +20,45 @@ final public class Song {
     public String getAuthor() {
         return author;
     }
-    // Setters for private variables
+
+    // (5) Returns a double representing minute length of the song. Length is stored in seconds
+    public double getMinutes() {
+        return secondsLength / 60.0; // 60 seconds in a minute
+    }
+
+    // (5) Returns a String representation of song length
+    public String getMinutesString() {
+        int minutes = secondsLength / 60;
+        int seconds = secondsLength % 60;
+        return String.format("%02d:%d", minutes, seconds);
+    }
+
+    // (2) Setters for private variables
     public void setTitle(String title) {
         this.title = title;
     }
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    /* (3) Returns a multiline formatted string describing the class in the format:
+        Song: "<TITLE>"
+        Author: <AUTHOR>
+        Length: <LENGTH> minutes
+        Year: <YEAR>
+     */
+    public String toMultilineString() {
+        return String.format("""
+                Song: "%s"
+                Author: %s
+                Length: %s
+                Year: %d""", getTitle(), getAuthor(), getMinutesString(), releaseYear);
+    }
+
+    /* (4) Returns a one line formatted description of the class in the format:
+        "<TITLE>", by <AUTHOR>, <LENGTH> min, y. <RELEASE YEAR>     */
+    public String toString() {
+        return String.format("\"%s\", by %s, %s, y. %d", getTitle(), getAuthor(), getMinutesString(), releaseYear);
     }
 }
