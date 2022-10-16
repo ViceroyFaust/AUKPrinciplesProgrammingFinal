cdf7963a6f7f1acd72af19b0f007bed7cb4d2cd5
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-10-16 21:31:07 +0300

    Optimise imports for SongCollection

--- src/SongCollection.java
+++ src/SongCollection.java
@@ -1,5 +1,8 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class SongCollection {
     private int count; // Storing number of elements
